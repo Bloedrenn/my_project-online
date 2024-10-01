449971e61b4d8b21ac2b3a609178b7158ed84433
Bloedrenn <bloedrenn@gmail.com>
2024-10-01 23:56:05 +0300

    ...

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    slug = models.SlugField(unique=True)
+    tags = models.JSONField(null=True, blank=True, default=list)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('post_by_slug', kwargs={'slug': self.slug})
+        # return f'/blog/post/{self.slug}/view/'
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from .dataset import dataset
+from .models import Post
 
 menu = [
     {"name": "Главная", "alias": "main"},
@@ -35,12 +36,22 @@ def about(request):
 
 
 def post_by_slug(request, slug):
-    post = [post for post in dataset if post['slug'] == slug]
+    # posts = Post.objects.all()
+    # post = [post for post in posts if post.slug == slug][0]
 
-    if not post:
-        return HttpResponse('404 - Пост не найден', status=404)
-    
-    context = post[0]
+    # if not post:
+    #     return HttpResponse('404 - Пост не найден', status=404)
+
+    post = get_object_or_404(Post, slug=slug)
+
+    context = {
+        'title': post.title,
+        'text': post.text,
+        'hashtags': post.tags,
+        'created_at': post.created_at,
+        'updated_at': post.updated_at
+    }
+
     context['menu'] = menu
     context['page_alias'] = 'blog'
     
