0d8a1d6557256d143ac1a36849c5f90fe116e417
Bloedrenn <bloedrenn@gmail.com>
2024-11-02 02:24:35 +0300

    Пример работы get и post в базовой View на примере AddCategoryView

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import blog, add_post, update_post, add_category, add_tag, update_category, preview_post, AboutView, post_by_slug, posts_by_category, posts_by_tag
+from .views import blog, add_post, update_post, AddCategoryView, add_tag, update_category, preview_post, AboutView, post_by_slug, posts_by_category, posts_by_tag
 
 urlpatterns = [
     path('', blog, name='blog'),
@@ -14,7 +14,7 @@ urlpatterns = [
     path("post/<slug:post_slug>/update/", update_post, name="update_post"),
     path('preview/', preview_post, name='preview_post'),
 
-    path("category/add/", add_category, name="add_category"),
+    path("category/add/", AddCategoryView.as_view(), name="add_category"),
     path("category/<slug:category_slug>/update/", update_category, name="update_category"),
 
     path("tag/add/", add_tag, name="add_tag"),
